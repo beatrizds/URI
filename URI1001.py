2689e86488419bbf96662485c501db2dd8522d23
beatrizds <beatrizds@unifeg.edu.br>
2019-05-07 20:15:28 -0300

    primeiros exercicios

---
+++ URI1001.py
@@ -0,0 +1,9 @@
+#exercicio 101 URI
+
+A = int (int(input)) #Recebe a entrada atravez de input, converte para int, e armazena em A
+B = input() #recebe a entrada atravez de input
+B = int(B) #converte B para int e armazena em A
+
+X = A + B #faz s oma entre A e B
+
+print('x =',X) #exibe X da forma proposta
---
+++ URI1003.py
@@ -0,0 +1,8 @@
+#exercicio URI 1003
+
+A = int(input())
+B = int(input())
+
+SOMA = A + B
+
+print("SOMA =",SOMA) 
